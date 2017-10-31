35031b1e48831c3e1299d4c8cfea1547b01aadea
NRothera <31472463+NRothera@users.noreply.github.com>
2017-10-31 17:47:33 +0000

    working, but need to tidy up

--- main.js
+++ main.js
@@ -1,45 +1,48 @@
+var player1Choice, play1,play0, randomPlay2
+
 var rockPaperScissors = ['rock','paper','scissors']
 var winner = true;
 var player1 = 'Nick';
-var player2 = 'Computer'
+var computer = 'Computer'
+var player1Score = 0;
+var computerScore = 0;
 
-var randomPlay2 = Math.floor(Math.random()*3)
-
-
-
-for (var i = 0; i < 10; i++) {
-  var player1Choice = prompt('Please choose 0 for ' + rockPaperScissors[0] + ', 1 for '+ rockPaperScissors[1] + ' or 2 for '+ rockPaperScissors[2])
-  randomPlay2 = Math.floor(Math.random()*3)
-  var play1 = rockPaperScissors[player1Choice]
-  var play2 = rockPaperScissors[randomPlay2]
+while (player1Score < 3 && computerScore < 3) {
+  init()
   if (play1 == play2){
     console.log("It's a draw! Try again")
-    continue
   }else if (play1 === 'scissors' && play2==='paper'){
     player1Wins()
-    break
   }else if (play2 === 'scissors' && play1==='paper'){
-    player2Wins()
-    break
+    computerWins()
   }else if (play1 === 'paper' && play2==="rock"){
     player1Wins()
-    break
   }else if (play2 === 'paper' && play1==='rock'){
-    player2Wins()
-    break
+    computerWins()
   }else if (play1 === 'scissors' && play2 === 'rock'){
-    player2Wins()
+    computerWins()
   }else if (play2 === 'scissors' && play1 === 'rock'){
     player1Wins()
   }
 }
 
 function player1Wins(){
-  alert(player1 + ' uses ' + play1 + ", " + player2 + ' uses ' + play2)
+  alert(player1 + ' uses ' + play1 + ", " + computer + ' uses ' + play2)
   alert(player1 + ' wins!')
+  player1Score +=1
+  alert("The scores are: player1 - " + player1Score +', Computer- '+ computerScore )
 }
 
-function player2Wins(){
-  alert(player1 + ' uses ' + play1 + ", " + player2 + ' uses ' + play2)
-  alert(player2 + ' wins!')
+function computerWins(){
+  alert(player1 + ' uses ' + play1 + ", " + computer + ' uses ' + play2)
+  alert(computer + ' wins!')
+  computerScore +=1
+  alert("The scores are: player1 - " + player1Score +', Computer - '+ computerScore )
+}
+
+function init(){
+  player1Choice = prompt('Please choose 0 for ' + rockPaperScissors[0] + ', 1 for '+ rockPaperScissors[1] + ' or 2 for '+ rockPaperScissors[2])
+  randomPlay2 = Math.floor(Math.random()*3)
+  play1 = rockPaperScissors[player1Choice]
+  play2 = rockPaperScissors[randomPlay2]
 }
