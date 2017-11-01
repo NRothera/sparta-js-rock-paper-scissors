025a468051ae3fcf75d951da248646b07c96f3ac
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-01 11:21:48 +0000

    new folder and text

--- main.js
+++ main.js
@@ -1,38 +1,46 @@
 var player1Choice, play1,play0, randomPlay2;
 
+// These are the variables used throughout the game
 var rockPaperScissors = ['rock','paper','scissors']
 var player1 = prompt('Please enter your name');
 var computer = 'Computer'
 var player1Score = 0;
 var computerScore = 0;
 
-while (player1Score < 3 && computerScore < 3) {
-  init();
-  if (play1 == play2){
-    alert("It's a draw! Try again")
-  }else if (play1 === 'scissors' && play2==='paper'){
-    player1Wins();
-  }else if (play2 === 'scissors' && play1==='paper'){
-    computerWins();
-  }else if (play1 === 'paper' && play2==="rock"){
-    player1Wins();
-  }else if (play2 === 'paper' && play1==='rock'){
-    computerWins();
-  }else if (play1 === 'scissors' && play2 === 'rock'){
-    computerWins();
-  }else if (play2 === 'scissors' && play1 === 'rock'){
-    player1Wins();
+init()
+
+//This is where the game takes place
+function init(){
+  while (player1Score < 3 && computerScore < 3) {
+    gameStart();
+    if (play1 == play2){
+      alert("It's a draw! Try again")
+    }else if (play1 === 'scissors' && play2==='paper'){
+      player1Wins();
+    }else if (play1 === 'paper' && play2==="rock"){
+      player1Wins();
+    }else if (play2 === 'scissors' && play1 === 'rock'){
+      player1Wins();
+    }else if (play2 === 'paper' && play1==='rock'){
+      computerWins();
+    }else if (play1 === 'scissors' && play2 === 'rock'){
+      computerWins();
+    }else if (play2 === 'scissors' && play1==='paper'){
+      computerWins();
+    }
+    winCheck();
   }
-  winCheck();
 }
 
-function init(){
+// This function stores the player choice and randomly selects the computer choice
+function gameStart(){
   player1Choice = prompt('Please choose rock, paper or scissors');
   randomPlay2 = Math.floor(Math.random()*3);
   play1 = player1Choice;
   play2 = rockPaperScissors[randomPlay2];
 }
 
+//This function performs when the human player wins
 function player1Wins(){
   alert(player1 + ' uses ' + play1 + ", " + computer + ' uses ' + play2);
   alert(player1 + ' wins!');
@@ -40,6 +48,7 @@ function player1Wins(){
   alert("The scores are: player1 - " + player1Score +', Computer- '+ computerScore );
 }
 
+//This function performs when the computer wins
 function computerWins(){
   alert(player1 + ' uses ' + play1 + ", " + computer + ' uses ' + play2);
   alert(computer + ' wins!');
@@ -47,6 +56,7 @@ function computerWins(){
   alert("The scores are: player1 - " + player1Score +', Computer - '+ computerScore );
 }
 
+//At the end of each round this function checks to see if anyone has won yet.
 function winCheck(){
   if (player1Score === 3){
     alert(player1 + ' wins the game!');
